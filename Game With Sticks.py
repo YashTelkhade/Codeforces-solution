a74087219f8ef325924f03f870d9e4d1128b4343
Yash Telkhade <59116181+YashTelkhade@users.noreply.github.com>
2020-06-08 13:11:57 +0530

    Create Game With Sticks.py

---
+++ Game With Sticks.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+if(a<b):
+    if(a%2==0):
+        print("Malvika")
+    else:
+        print("Akshat")
+else:
+    if(b%2==0):
+        print("Malvika")
+    else:
+        print("Akshat")
+    
